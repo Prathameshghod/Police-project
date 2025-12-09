6c3bce4121d2b66af1552334b496eec137ab2d8d
Shreynik00 <jainsy@rknec.edu>
2025-12-09 16:29:08 +0530

    Update Login component with credentials and styling

--- react-tailwind-app/src/pages/login.tsx
+++ react-tailwind-app/src/pages/login.tsx
@@ -1,6 +1,24 @@
+import { Link } from "react-router-dom";
+
+
 function Login() {
+
+  const credentials = {
+    username: "shreynik",
+    password: "12345",
+  };
+
     return (
-      <h1>Login Page</h1>
+      <div className="text-white text-6xl flex justify-center items-center h-screen bg-black">
+      Login Page
+
+    
+      
+      <Link to="/service" state={credentials}>
+      <button>Go to service </button>
+    </Link>
+     
+      </div>
     );
   }
   
