a875986fa620f5252b75199b51851e35757c8e1b
Shreynik Jain <jainsy@rknec.edu>
2026-01-03 02:47:02 +0530

    updated thelogin

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import "./css/Login.css";
+import "./css/login.css";
 
 export default function Login() {
   const [usernameOrEmail, setUsernameOrEmail] = useState("");
